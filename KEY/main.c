d236057a3a3641944e86961171259dcd9d9c408f
hzzqq1 <Joe-Huangzz@outlook.com>
2026-07-07 11:02:24 +1000

    Add new files

---
+++ KEY/main.c
@@ -0,0 +1,30 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "key.h"
+
+void app_main() 
+{
+  led_init();
+
+  key_init();
+  uint8_t key;
+  while(1)
+  {
+    key = key_scan(0);
+
+    switch (key)
+    {
+      case BOOT_PRES:
+      {
+        LED_TOGGLE();
+        break;
+      }
+      default:
+        break;
+    }
+    vTaskDelay(10);
+
+  }
+  
+}
